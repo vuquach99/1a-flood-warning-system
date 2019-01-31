24950f60668a8ea6b2a4b3ec2113e57f77beb383
Myeong Jin <mjc284@cam.ac.uk>
2019-01-31 21:08:43 +0000

    fixed some errors

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,25 +1,5 @@
 
-def sorted_by_key(x, i, reverse=False):
-    """For a list of lists/tuples, return list sorted by the ith
-    component of the list/tuple, E.g.
-
-    Sort on first entry of tuple:
-
-      > sorted_by_key([(1, 2), (5, 1]), 0)
-      >>> [(1, 2), (5, 1)]
-
-    Sort on second entry of tuple:
-
-      > sorted_by_key([(1, 2), (5, 1]), 1)
-      >>> [(5, 1), (1, 2)]
-
-    """
-
-    # Sort by distance
-    def key(element):
-        return element[i]
-
-    return sorted(x, key=key, reverse=reverse)
+from .utils import sorted_by_key
 
 def stations_level_over_threshold(stations, tol):
     stations_list = []
