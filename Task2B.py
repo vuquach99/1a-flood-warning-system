2293083165d9e7029f89640b3ebffd1933debbaf
Myeong Jin <mjc284@cam.ac.uk>
2019-01-31 20:57:21 +0000

    made descriptions for each task

--- Task2B.py
+++ Task2B.py
@@ -2,10 +2,15 @@ from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
 
 def run():
+    #Build a list of stations
     stations = build_station_list()
+    #update water levels
     update_water_levels(stations)
+    #set tolerance level
     tolerance = 0.8
+    #Make a list of all stations with relative water levels above tolerance
     list_of_stations = stations_level_over_threshold(stations, tolerance)
+    #show each station name and relative water level
     for i in list_of_stations:
         print(i[0], i[1])
 
--- Task2C.py
+++ Task2C.py
@@ -2,9 +2,13 @@ from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_rel_level
 
 def run():
+    #Build a list of stations
     stations = build_station_list()
+    #update water levels
     update_water_levels(stations)
+    #Build a list of 10 stations with highest relative water levels
     list_of_stations = stations_highest_rel_level(stations, 10)
+    #show each stations and relative water level
     for i in list_of_stations:
         print(i[0], i[1])
 
