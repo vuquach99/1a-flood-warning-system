97bef7afe4371cfdcebf1b24946c6a8592bc6545
Vu Quach <hvq20@cam.ac.uk>
2019-02-03 02:17:59 +0000

    Updated Task 2E, needs fixing

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -5,19 +5,20 @@ def plot_water_levels(station, dates, levels):
     """Task 2E: plots water level against time"""
     
     #Assign variables
-    range_high = station.typical_range[1]
-    range_low = station.typical_range[0]
+    range_high = [station.typical_range[1]]*len(dates)
+    range_low = [station.typical_range[0]]*len(dates)
 
     # Plot
-    plt.plot(dates, levels, label="$water level$")
-    plt.plot(dates, range_high, label="$typical high$")
-    plt.plot(dates, range_low, label="$typical low$")
+    plt.plot(dates, levels, label="Water Level")
+    plt.plot(dates, range_high, label="Typical High")
+    plt.plot(dates, range_low, label="Typical Low")
 
-    # Add axis labels, rotate date labels and add plot title
-    plt.xlabel('date')
-    plt.ylabel('water level (m)')
+    # Add axis labels, add axis legend, rotate date labels and add plot title
+    plt.xlabel('Date')
+    plt.ylabel('Water Level (m)')
+    plt.legend()
     plt.xticks(rotation=45)
-    plt.title(station)
+    plt.title(station.name)
 
     # Display plot
     plt.tight_layout()  # This makes sure plot does not cut off date labels
--- test_plot.py
+++ test_plot.py
@@ -14,6 +14,7 @@ def test_plot():
 
     t = [datetime(2016, 12, 30), datetime(2016, 12, 31), datetime(2017, 1, 1), datetime(2017, 1, 2), datetime(2017, 1, 3), datetime(2017, 1, 4), datetime(2017, 1, 5)]
     level = [0.2, 0.7, 0.95, 0.92, 1.02, 0.91, 0.64]
-    plot = plot_water_levels(station_cam, t, level)
+
+    plot_water_levels(station_cam, t, level)
+
     
-    return plot 
