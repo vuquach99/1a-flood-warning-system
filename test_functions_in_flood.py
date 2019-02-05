4a3d78748dcda6d4b2d3face57c779f5875667fe
Myeong Jin <mjc284@cam.ac.uk>
2019-02-05 20:53:46 +0000

    fixed test for functions in 'flood'

--- test_functions_in_flood.py
+++ test_functions_in_flood.py
@@ -1,17 +1,27 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
 
-def test_stationsleveloverthreshold():
+def test_stationsleveloverthreshold1():
     stations = build_station_list()
     update_water_levels(stations)
     assert stations_level_over_threshold(stations, 100000) == []
-test_stationsleveloverthreshold()
 
-def test_stationhighestrellevel():
+def test_stationsleveloverthreshold2():
     stations = build_station_list()
-    #update water levels
     update_water_levels(stations)
-    #Build a list of 10 stations with highest relative water levels
-    assert stations_highest_rel_level(stations, 0) == [3]
+    assert stations_level_over_threshold(stations, 0) != []
 
-test_stationhighestrellevel()
+def test_stationhighestrellevel1():
+    stations = build_station_list()
+    update_water_levels(stations)
+    assert stations_highest_rel_level(stations, 0) == []
+
+def test_stationhighestrellevel2():
+    stations = build_station_list()
+    update_water_levels(stations)
+    assert stations_highest_rel_level(stations, 10) != []
+
+test_stationsleveloverthreshold1()
+test_stationhighestrellevel2()
+test_stationsleveloverthreshold1()
+test_stationsleveloverthreshold2()
